97d7d61b7bf6e5e807ea44cf4169d2e903e46024
kjb4494 <rlagnltn1357@gmail.com>
2019-02-08 18:47:36 +0900

    보기 불편해서 클래스들 이사감 :)

--- February_2w/stack_min.py
+++ February_2w/stack_min.py
@@ -6,8 +6,44 @@ push, pop, min연산은 모두 O(1) 시간에 동작해야 한다.
 힌트 : #27, #59, #78
 """
 
-from Stack import StackMin
+from Stack import Stack, Node
 from random import randint
+
+
+# StackMin: 기본적인 기능만 가진 Stack 객체를 상속받은 solution용 객체
+class StackMin(Stack):
+    def __init__(self):
+        super().__init__()
+        self.min_stack = []
+        self.current_min = None
+
+    def push(self, data):
+        self.min_stack.append(self.current_min)
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
+        if self.current_min is None or self.current_min > data:
+            self.current_min = data
+
+    def pop(self):
+        if self.is_empty():
+            return
+        self.current_min = self.min_stack.pop()
+        self.head = self.head.next
+
+    def min(self):
+        if self.current_min is not None:
+            return self.current_min
+        else:
+            return "min error: stack is empty!"
+
+    def show_all(self):
+        result = ""
+        start = self.head
+        while start is not None:
+            result += str(start.data) + " "
+            start = start.next
+        return result
 
 
 def test():
@@ -17,7 +53,6 @@ def test():
     print(pop_num)
 
 
-# StackMin: 기본적인 기능만 가진 Stack 객체를 상속받은 solution용 객체
 def main_code():
     stack = StackMin()
     for i in range(10):
--- February_2w/three_in_one.py
+++ February_2w/three_in_one.py
@@ -8,7 +8,53 @@ p.147 [스택과 큐]
 
 from random import randint
 from decorators import time_measurement
-from Stack import ThreeInOneStack
+from Stack import Stack, Node
+
+
+# ThreeInOneStack: 기본적인 기능만 가진 Stack 객체를 상속받은 solution용 객체
+class ThreeInOneStack(Stack):
+    def __init__(self, arr):
+        super().__init__()
+        self.max_size = len(arr)
+        self.current_size = 0
+        for data in arr:
+            self.push(data)
+
+    def attach_arr(self, arr):
+        self.max_size += len(arr)
+        for data in arr:
+            self.push(data)
+
+    def push(self, data):
+        if self.is_full():
+            return
+        else:
+            new_node = Node(data)
+            new_node.next = self.head
+            self.head = new_node
+            self.current_size += 1
+            # print("push: {} --> {}".format(self.head, self.head.data))
+
+    def pop(self):
+        if self.is_empty():
+            return
+        # print("pop: {} --> {}".format(self.head, self.head.data))
+        self.head = self.head.next
+        self.current_size -= 1
+
+    def is_full(self):
+        if self.current_size >= self.max_size:
+            print("push error: stack is full!")
+            return True
+        return False
+
+    def show_all(self):
+        result = ""
+        start = self.head
+        while start is not None:
+            result += str(start.data) + " "
+            start = start.next
+        return result
 
 
 def slice_arr(arr, n):
@@ -16,14 +62,13 @@ def slice_arr(arr, n):
         yield arr[i:i + n]
 
 
-# ThreeInOneStack: 기본적인 기능만 가진 Stack 객체를 상속받은 solution용 객체
 @time_measurement
 def solution(arr):
-    if len(arr) < 3:                                    # Constant 1
+    if len(arr) < 3:                                # Constant 1
         print("Array too small...")
         return None
-    arr_scissors = slice_arr(arr, len(arr) // 3)        # Constant 1
-    stack_01 = ThreeInOneStack(next(arr_scissors))      # Constant n^2
+    arr_scissors = slice_arr(arr, len(arr) // 3)    # Constant 1
+    stack_01 = ThreeInOneStack(next(arr_scissors))  # Constant n^2
     stack_02 = ThreeInOneStack(next(arr_scissors))
     stack_03 = ThreeInOneStack(next(arr_scissors))
     try:
