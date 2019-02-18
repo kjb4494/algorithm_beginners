f4d9694640ab674cbd36ef8429b6937d1a3e62dd
kjb4494 <rlagnltn1357@gmail.com>
2019-02-18 11:29:15 +0900

    4.1 주석 추가

--- February_3w/path_between_nodes.py
+++ February_3w/path_between_nodes.py
@@ -21,18 +21,25 @@ example_graph = [[0, 0, 1, 0, 0, 1, 0, 0, 0],  # 0
 
 @time_measurement
 def solution(graph, src, dst):
+    # 경로를 추적할 노드의 대기 배열
     nodes_to_visit = [src]
+    # 경로 추적을 마친 노드 배열
     visited_node = []
     while nodes_to_visit:
+        # 경로 추적이 끝난 노드일 경우 대기 배열에서 삭제하고 다음으로 넘어감
+        # 같은 작업으로 인한 연산 처리량을 줄이고 무한루프를 방지
         if nodes_to_visit[0] in visited_node:
             del nodes_to_visit[0]
             continue
+        # 다음 노드 경로를 추적하고 목적지 노드에 도달하면 True를 반환함
         for i in range(len(graph[nodes_to_visit[0]])):
             if graph[nodes_to_visit[0]][i] >= 1:
                 if i == dst:
                     return True
                 else:
+                    # 목적지 노드가 아닐 경우 대기 배열에 추가
                     nodes_to_visit.append(i)
+        # 추적이 끝난 노드로 배열에 추가하고 대기 배열에서 삭제
         visited_node.append(nodes_to_visit[0])
         del nodes_to_visit[0]
     return False
@@ -40,7 +47,6 @@ def solution(graph, src, dst):
 
 def main_code():
     i, j = randint(0, 8), randint(0, 8)
-
     if solution(example_graph, i, j):
         print("{}에서 {}로 가는 경로는 존재한다.".format(i, j))
     else:
