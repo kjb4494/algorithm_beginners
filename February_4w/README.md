bc3f5db9b84ed1aa77de7300f9019faaf8f13473
kjb4494 <rlagnltn1357@gmail.com>
2019-02-22 10:24:42 +0900

    4.4 문제 풀이 해결

--- February_4w/README.md
+++ February_4w/README.md
@@ -6,5 +6,8 @@
     - 생성한 이차배열을 이용해 연결리스트 객체들을 생성함
   
   * 균형 확인 - check_balance
+    - 이진 탐색 트리 객체 재활용(February_3w.binary_search_tree)
+    - 전위 순회 알고리즘에서 깊이가 1인 노드부터 왼쪽과 오른쪽으로 갈리는 특성을 이용함
+    - 루트 노드의 왼쪽 노드가 Null이면 왼쪽 트리의 깊이는 0임
   
   * BST 검증 - BST_verification
--- February_4w/check_balance.py
+++ February_4w/check_balance.py
@@ -4,3 +4,99 @@
 오른쪽 부분 트리의 높이의 차이가 최대 하나인 트리를 의미한다.
 """
 
+from binary_search_tree import BinarySearchTree
+from random import randint
+from decorators import time_measurement
+
+
+class Solution(BinarySearchTree):
+    def __init__(self):
+        super().__init__()
+        self.left_or_right = False
+
+    # 이진트리의 균형 정보를 반환하는 함수
+    def get_balance_info(self):
+        # 왼쪽 노드들과 오른쪽 노드들의 최대 깊이 정보를 제어할 수 있는 객체
+        # 가변할 수 있는 글로벌 변수의 역할
+        class DepthInfo:
+            LEFT = False
+            RIGHT = True
+
+            def __init__(self):
+                self._left_depth = 0
+                self._right_depth = 0
+                self._current_location = None
+
+            def switching_current_location(self):
+                if self._current_location == DepthInfo.LEFT:
+                    self._current_location = DepthInfo.RIGHT
+                else:
+                    self._current_location = DepthInfo.LEFT
+
+            def get_current_location(self):
+                return self._current_location
+
+            def set_left_depth(self, left_depth):
+                self._left_depth = left_depth
+
+            def set_right_depth(self, right_depth):
+                self._right_depth = right_depth
+
+            def get_left_depth(self):
+                return self._left_depth
+
+            def get_right_depth(self):
+                return self._right_depth
+
+        def _get_balance_info(root, depth, di):
+            if root is None:
+                pass
+            else:
+                # 깊이가 1일 경우 최초의 분기점
+                if depth == 1:
+                    di.switching_current_location()
+
+                # 왼쪽, 오른쪽 방향에 따라서 깊이가 갱신되면 누적
+                if di.get_current_location() == di.LEFT and depth > di.get_left_depth():
+                    di.set_left_depth(depth)
+                elif di.get_current_location() == di.RIGHT and depth > di.get_right_depth():
+                    di.set_right_depth(depth)
+                depth += 1
+                _get_balance_info(root.left, depth, di)
+                _get_balance_info(root.right, depth, di)
+
+        di = DepthInfo()
+        # 루트 노드의 왼쪽 방향 노드가 없으면 깊이 정보를 오른쪽으로 스위칭
+        # 왼쪽 방향 트리의 높이가 0이기 때문
+        if self.root.left is None:
+            di.switching_current_location()
+        # 전위 순회 알고리즘을 이용한 재귀탐색 시작
+        _get_balance_info(self.root, 0, di)
+        depth_compare = abs(di.get_right_depth() - di.get_left_depth())
+        return depth_compare <= 1, di
+
+
+@time_measurement
+def solution(bst):
+    return bst.get_balance_info()
+
+
+def main_code():
+    array = list(set([randint(1, 400) for i in range(randint(10, 40))]))
+    bst = Solution()
+    for x in array:
+        bst.insert(x)
+    print("이진 탐색 트리(전위 순회):", end=" ")
+    bst.pre_order_traversal()
+    is_balance, depth_info = solution(bst)
+    if is_balance:
+        print("균형 잡힌 트리입니다.")
+    else:
+        print("균현 잡힌 트리가 아닙니다.")
+    print("왼쪽 높이: {} / 오른쪽 높이: {}".format(depth_info.get_left_depth(), depth_info.get_right_depth()))
+
+
+if __name__ == "__main__":
+    for i in range(20):
+        main_code()
+        print()
