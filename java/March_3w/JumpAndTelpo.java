4a216299f61a7cc720d256bfc8011215078cecfd
kjb4494 <rlagnltn1357@gmail.com>
2019-03-26 18:17:36 +0900

    java 배우려고 java로 풀어봄

---
+++ java/March_3w/JumpAndTelpo.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        String binary_n = Integer.toBinaryString(n);
+        for (int i=0; i < binary_n.length(); i++){
+            if (binary_n.charAt(i) == '1'){
+                answer += 1;
+            }
+        }
+        return answer;
+    }
+}
