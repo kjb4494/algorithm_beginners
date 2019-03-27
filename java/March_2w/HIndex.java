4182904ca5df20d0bdebd5f056f98e883ebe4021
kjb4494 <rlagnltn1357@gmail.com>
2019-03-27 10:24:03 +0900

    java 배우려고 java로 풀어봄

---
+++ java/March_2w/HIndex.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] citations) {
+        Arrays.sort(citations);
+        reverseArrayInt(citations);
+        int answer = 0;
+        int min_number;
+        for (int i=0; i<citations.length; i++){
+            if (i + 1 > citations[i]) {
+                min_number = citations[i];
+            }
+            else{
+                min_number = i + 1;
+            }
+            if (min_number > answer){
+                answer = min_number;
+            }
+        }
+        return answer;
+    }
+
+    private static void reverseArrayInt(int[] array) {
+        int temp;
+        for (int i = 0; i < array.length / 2; i++) {
+            temp = array[i];
+            array[i] = array[(array.length - 1) - i];
+            array[(array.length - 1) - i] = temp;
+        }
+    }
+}
