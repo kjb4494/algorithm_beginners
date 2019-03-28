0d53341d0220f5f52404e5873178f6f3c041586d
kjb4494 <rlagnltn1357@gmail.com>
2019-03-28 18:46:52 +0900

    HashSet으로 바꿔서 풀어봄

--- java/src/March_4w/EnglishEndToEnd.java
+++ java/src/March_4w/EnglishEndToEnd.java
@@ -2,29 +2,21 @@
 
 package March_4w;
 
+import java.util.HashSet;
+
 public class EnglishEndToEnd {
     protected int[] solution(int n, String[] words){
         int[] answer = new int[] {0, 0};
+        HashSet<String> word_set = new HashSet<String>(){{add(words[0]);}};
         for (int i=1; i < words.length; i++){
-            if (!is_end_to_end_word(i, words) || is_contained_word(i, words)){
+            if (words[i].charAt(0) != words[i-1].charAt(words[i-1].length()-1) ||
+                    word_set.contains(words[i])){
                 answer[0] = i % n + 1;
                 answer[1] = i / n + 1;
                 break;
             }
+            word_set.add(words[i]);
         }
         return answer;
-    }
-
-    private boolean is_end_to_end_word(int n, String[] words){
-        return words[n].charAt(0) == words[n-1].charAt(words[n-1].length()-1);
-    }
-
-    private boolean is_contained_word(int n, String[] words){
-        for (int i=0; i < n; i++){
-            if (words[i].equals(words[n])){
-                return true;
-            }
-        }
-        return false;
     }
 }
